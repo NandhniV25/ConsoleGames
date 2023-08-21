5509ebb9e59d224f95348a11768862c54b3588be
Nandhni G A <nandhni.25@gmail.com>
2023-08-21 10:14:49 +0700

    creation

---
+++ SnakeGame/SnakeGame/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeGame
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Board board = new Board(20,20);
+            var taskKeys = new Task(() => board.Read());
+            taskKeys.Start();
+            board.Play();
+        }
+    }
+}
